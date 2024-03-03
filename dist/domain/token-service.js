3e661ffec14e4b51a2b5b66c10f75fd662d0ea32
VadimDgahm <dgahm@mail.ru>
2024-03-03 14:47:23 +0300

    change url

--- dist/domain/token-service.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenService = void 0;
-exports.tokenService = {};
--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://market-manager-six.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
